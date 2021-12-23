cc0e34725853792c08169fb548848603f008b9ba
Wamza <gabriel.hoche@orange.fr>
2021-12-23 20:07:46 +0100

    Smooth movement for the camera + Wall PhysicMaterial

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,8 +7,11 @@ public class FollowCamera : MonoBehaviour
     public GameObject player;
     public Vector3 offset;
 
+    private Vector3 camVelocity = Vector3.zero;
+
     private void Update()
     {
-        transform.position = player.transform.position + offset;
+        Vector3 targetPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camVelocity, 0.1f);
     }
 }
