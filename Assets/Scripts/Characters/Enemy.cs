7b7b3644dae8ab7eac344c721862d1a86d22aec1
Wamza <gabriel.hoche@orange.fr>
2022-01-11 00:46:53 +0100

    weapon + enemy hitboxes

---
+++ Assets/Scripts/Characters/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth;
+
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject weapon;
+
+    private bool isAttacking;
+
+    private void Start()
+    {
+        isAttacking = false;
+        weapon.SetActive(false);
+    }
+
+    private void Update()
+    {
+        bool attackInput = Input.GetKeyDown(KeyCode.F);
+        if(!isAttacking && attackInput)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+        weapon.SetActive(true);
+
+        yield return new WaitForSeconds(0.2f);
+
+        isAttacking = false;
+        weapon.SetActive(false);
+    }
+
+}
